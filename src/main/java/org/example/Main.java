a2be7b367f9d0a65480c03edd964bf5812235853
Marta Liushniak <marta.liushniak@yahoo.com>
2023-01-11 16:38:31 +0200

    rock-paper-scissors solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(rps("paper", "scissors"));
+    }
 
+    public static String rps(String p1, String p2) {
+        return p1.equals(p2) ? "Draw!" : (p1.equals("scissors") && p2.equals("paper")
+                || p1.equals("rock") && p2.equals("scissors")
+                ||(p1.equals("paper") && p2.equals("rock")) ? "Player 1 won!" : "Player 2 won!");
     }
 }
